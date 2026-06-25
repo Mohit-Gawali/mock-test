21a7e4e8a49524129394cc178708422c7f7f658c
mohitgawali2005 <mohitsharadraogawali@gmail.com>
2026-06-25 10:07:43 +0530

    Fix favicon 404 error

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -18,6 +18,11 @@ def exam_page():
 @app.route('/score')
 def score_page():
     return render_template('score.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
